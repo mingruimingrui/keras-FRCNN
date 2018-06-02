f0745e582ee06538a56268eb695675219395a21e
Wang Ming Rui <mingruimingrui@hotmail.com>
2018-06-02 14:56:29 +0800

    added setup.py script

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name='keras-pipeline',
+    version='0.1',
+    description='A collection of deep learning related models and tools written in keras and tensorflow',
+    url='https://github.com/mingruimingrui/keras-pipeline',
+    author='Wang Ming Rui',
+    author_email='mingruimingrui@hotmail.com',
+    packages=['keras_pipeline']
+)

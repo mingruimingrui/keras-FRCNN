75324c8a5abac6e478e318cf550fd2772ea32a17
Wang Ming Rui <mingruimingrui@hotmail.com>
2018-06-15 23:59:31 +0800

    changed the way resnet backbone works since keras_resnet does not allow none Input tensor as input_tensor

--- keras_pipeline/models/resnet.py
+++ keras_pipeline/models/resnet.py
@@ -12,7 +12,7 @@ custom_objects.update(keras_resnet.custom_objects)
 def ResNet18(input_tensor, include_top=True, freeze_backbone=False):
     """ Loads a resnet 18 model with preprocessing """
     x = layers.ResNetPreprocess()(input_tensor)
-    resnet_model = keras_resnet.models.ResNet18(x, include_top=include_top, freeze_bn=True)
+    resnet_model = keras_resnet.models.ResNet18(keras.Input(tensor=x), include_top=include_top, freeze_bn=True, name='resnet18_')
 
     if freeze_backbone:
         for layer in resnet_model.layers:
@@ -20,8 +20,8 @@ def ResNet18(input_tensor, include_top=True, freeze_backbone=False):
 
     return keras.Model(
         inputs = input_tensor,
-        outputs = resnet_model.output,
-        name = 'vgg16'
+        outputs = resnet_model(input_tensor),
+        name = 'resnet18'
     )
 
 
@@ -34,25 +34,27 @@ def ResNet18Backbone(input_tensor, freeze_backbone=False):
     C1 = resnet_model.get_layer(relu_layer_names[0]).output
     C2, C3, C4, C5 = resnet_model.output
 
-    backbone = keras.Model(
+    return keras.Model(
         inputs = resnet_model.input,
         outputs = [C1, C2, C3, C4, C5],
         name = 'resnet18_backbone'
     )
 
-    return backbone
-
 
 def ResNet34(input_tensor, include_top=True, freeze_backbone=False):
     """ Loads a resnet 34 model with preprocessing """
     x = layers.ResNetPreprocess()(input_tensor)
-    resnet_model = keras_resnet.models.ResNet34(x, include_top=include_top, freeze_bn=True)
+    resnet_model = keras_resnet.models.ResNet34(keras.Input(tensor=x), include_top=include_top, freeze_bn=True, name='resnet34_')
 
     if freeze_backbone:
         for layer in resnet_model.layers:
             layer.trainable = False
 
-    return resnet_model
+    return keras.Model(
+        inputs = input_tensor,
+        outputs = resnet_model(input_tensor),
+        name = 'resnet34'
+    )
 
 
 def ResNet34Backbone(input_tensor, freeze_backbone=False):
@@ -64,25 +66,27 @@ def ResNet34Backbone(input_tensor, freeze_backbone=False):
     C1 = resnet_model.get_layer(relu_layer_names[0]).output
     C2, C3, C4, C5 = resnet_model.output
 
-    backbone = keras.Model(
+    return keras.Model(
         inputs = resnet_model.input,
         outputs = [C1, C2, C3, C4, C5],
         name = 'resnet34_backbone'
     )
 
-    return backbone
-
 
 def ResNet50(input_tensor, include_top=True, freeze_backbone=False):
     """ Loads a resnet 50 model with preprocessing """
     x = layers.ResNetPreprocess()(input_tensor)
-    resnet_model = keras_resnet.models.ResNet50(x, include_top=include_top, freeze_bn=True)
+    resnet_model = keras_resnet.models.ResNet50(keras.Input(tensor=x), include_top=include_top, freeze_bn=True, name='resnet50_')
 
     if freeze_backbone:
         for layer in resnet_model.layers:
             layer.trainable = False
 
-    return resnet_model
+    return keras.Model(
+        inputs = input_tensor,
+        outputs = resnet_model(input_tensor),
+        name = 'resnet50'
+    )
 
 
 def ResNet50Backbone(input_tensor, freeze_backbone=False):
@@ -94,10 +98,8 @@ def ResNet50Backbone(input_tensor, freeze_backbone=False):
     C1 = resnet_model.get_layer(relu_layer_names[0]).output
     C2, C3, C4, C5 = resnet_model.output
 
-    backbone = keras.Model(
+    return keras.Model(
         inputs = resnet_model.input,
         outputs = [C1, C2, C3, C4, C5],
         name = 'resnet50_backbone'
     )
-
-    return backbone

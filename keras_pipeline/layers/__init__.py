11d6e210ec91f5c5099d5d0785f954a12e7bd79e
Wang Ming Rui <mingruimingrui@hotmail.com>
2018-06-15 23:11:16 +0800

    split preprocess into a separate file

--- keras_pipeline/layers/__init__.py
+++ keras_pipeline/layers/__init__.py
@@ -1,3 +1,4 @@
 from ._misc import ResizeTo
 from ._detections import Anchors, RegressBoxes, ClipBoxes
 from ._filter_detections import FilterDetections
+from ._preprocessing import InceptionPreprocess, ResNetPreprocess
--- keras_pipeline/layers/_misc.py
+++ keras_pipeline/layers/_misc.py
@@ -1,4 +1,3 @@
-import numpy as np
 import keras
 from .. import backend
 
@@ -13,36 +12,3 @@ class ResizeTo(keras.layers.Layer):
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0][0],) + input_shape[1][1:3] + (input_shape[0][-1],)
-
-
-class InceptionPreprocess(keras.layers.Layer):
-    """Performs preprocessing for a inception backbone"""
-    def __init__(self, *args, **kwargs):
-        _INCEPTION_SCALE = 1 / 127.5
-        _INCEPTION_BIAS  = -1
-        self.scale = keras.backend.constant(_INCEPTION_SCALE)
-        self.bias  = keras.backend.constant(_INCEPTION_BIAS)
-        super(InceptionPreprocess, self).__init__(*args, **kwargs)
-
-    def call(self, inputs, **kwargs):
-        x = inputs * self.scale + self.bias
-        return x
-
-    def compute_output_shape(self, input_shape):
-        return input_shape
-
-
-class ResNetPreprocess(keras.layers.Layer):
-    """Performs preprocessing for a resnet backbone"""
-    def __init__(self, *args, **kwargs):
-        _RESNET_MEAN = np.array([103.939, 116.779, 123.68], keras.backend.floatx())
-        self.bias = keras.backend.constant(-_RESNET_MEAN)
-        super(ResNetPreprocess, self).__init__(*args, **kwargs)
-
-    def call(self, inputs, **kwargs):
-        x = inputs[..., ::-1]
-        x = keras.backend.bias_add(x, self.bias)
-        return x
-
-    def compute_output_shape(self, input_shape):
-        return input_shape
